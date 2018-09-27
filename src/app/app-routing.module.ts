02d3a8e198b8c780f18bbc1bbea0887f8d9bc6a8
Seong Min Park <sm7park@edu.uwaterloo.ca>
2018-09-26 21:15:37 -0400

    some progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,23 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {MainComponent} from "./main/main.component";
 import {SignUpComponent} from "./sign-up/sign-up.component";
+import {ReadComponent} from "./main/read/read.component";
+import {WriteComponent} from "./main/write/write.component";
+import {SettingComponent} from "./main/setting/setting.component";
+import {HelpComponent} from "./main/help/help.component";
 
 const routes: Routes = [
-  {path: '', component: MainComponent},
+  {
+    path: '', component: MainComponent, children: [
+      {path: 'read', component: ReadComponent},
+      {path: 'write', component: WriteComponent},
+      {path: 'setting', component: SettingComponent},
+      {path: 'help', component: HelpComponent}
+    ]
+  },
   {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignUpComponent}
+  {path: 'signup', component: SignUpComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,114 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
 import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
   MatButtonModule,
+  MatButtonToggleModule,
   MatCardModule,
-  MatFormFieldModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
   MatInputModule,
-  MatProgressBarModule
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
 } from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {MainComponent} from './main/main.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {SettingComponent} from './main/setting/setting.component';
+import {WriteComponent} from './main/write/write.component';
+import {ReadComponent} from './main/read/read.component';
+import {CdkTableModule} from "@angular/cdk/table";
+import {CdkTreeModule} from "@angular/cdk/tree";
+import {HelpComponent} from './main/help/help.component';
 
+@NgModule({
+  exports: [
+    CdkTableModule,
+    CdkTreeModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+  ],
+  declarations: [HelpComponent]
+})
+export class DemoMaterialModule {
+}
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MainComponent,
-    SignUpComponent
+    SignUpComponent,
+    SettingComponent,
+    WriteComponent,
+    ReadComponent
   ],
   imports: [
     BrowserModule,
-    MatCardModule,
-    MatInputModule,
-    MatFormFieldModule,
     BrowserAnimationsModule,
-    MatButtonModule,
     AppRoutingModule,
-    MatProgressBarModule
+    FontAwesomeModule,
+    DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
